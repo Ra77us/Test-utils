5dc552ecc6195ff9cd0b31d2b7b2bf897c782686
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2024-05-03 12:28:11 +0200

    test3

--- src-for-commons/src/main/java/com/kubiki/controller/sample/dto/ScaleHeavyFlowActionArgs.java
+++ src-for-commons/src/main/java/com/kubiki/controller/sample/dto/ScaleHeavyFlowActionArgs.java
@@ -1,12 +1,16 @@
 package com.kubiki.controller.sample.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class ScaleHeavyFlowActionArgs {
     int upstreamThreads;
     String upstreamService;
-    int downstreamCpu;
+    double downstreamCpu;
     String downstreamPod;
     String downstreamNamespace;
 }
