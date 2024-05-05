3e98633c7ed8f5e68836320a3d10f8f9fcac1ba0
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2024-05-05 18:16:31 +0200

    test1

--- manual-invoker/src/main/java/com/example/droolsprototype/Controller.java
+++ manual-invoker/src/main/java/com/example/droolsprototype/Controller.java
@@ -17,8 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toCollection;
 
 @RestController
 @RequestMapping("/")
@@ -28,7 +34,7 @@ public class Controller {
     private final ActionInvoker actionInvoker;
 
     @GetMapping("test6")
-    public void getMapping() {
+    public void test6() {
         Integer check = new RestTemplate().getForObject("http://149.156.182.229:32222/test/success-counter", Integer.class);
         System.out.println(check);
         for (int retry = 0; retry < 6; retry++) {
@@ -45,5 +51,52 @@ public class Controller {
                 System.out.println(finalRetry + "," + fail + "," + res);
             });
         }
+    }
+
+    @GetMapping("test1")
+    public void test1() {
+        int n = 10;
+        Integer check = new RestTemplate().getForObject("http://149.156.182.229:32222/test/counter", Integer.class);
+        System.out.println(check);
+        List<ActionScheduleRequest> t1 =  getActions(20, 0, 0, 7).collect(toCollection(ArrayList::new));
+        performTests("t1", n, t1, 10);
+//
+//        List<ActionScheduleRequest> t2 = IntStream.range(0, 100).mapToObj(i -> getActions(1, i, i / 10, 25))
+//                .flatMap(Function.identity()).toList();
+//        performTests("t2", n, t2, 25);
+//
+//        List<ActionScheduleRequest> t3 = IntStream.range(0, 100).mapToObj(i -> getActions(1, i, (i / 10) * 2, 30))
+//                .flatMap(Function.identity()).toList();
+//        performTests("t3", n, t3, 30);
+//
+//        List<ActionScheduleRequest> t4 = IntStream.range(0, 100).mapToObj(i -> getActions(1, i, i / 4, 35))
+//                .flatMap(Function.identity()).toList();
+//        performTests("t4", n, t4, 35);
+//
+//        List<ActionScheduleRequest> t5 = IntStream.range(0, 100).mapToObj(i -> getActions(1, i, i, 105))
+//                .flatMap(Function.identity()).toList();
+//        performTests("t5", n, t5, 105);
+    }
+
+    private Stream<ActionScheduleRequest> getActions(int count, int priority, int delay, long window) {
+        return NumberAndDelayActionFactory.getActions(count, priority, delay, window);
+    }
+
+    private void performTests(String name, int n, List<ActionScheduleRequest> actions, int sleep) {
+        System.out.println(name);
+        for (int i=0; i<n; i++) {
+            performTest(actions, sleep);
+        }
+    }
+
+    private void performTest(List<ActionScheduleRequest> actions, int sleep) {
+        Collections.shuffle(actions);
+        actionInvoker.invokeBatchViaRabbit(actions);
+        try {
+            Thread.sleep(sleep * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(new RestTemplate().getForObject("http://149.156.182.229:32222/test/counter", Integer.class));
     }
 }
---
+++ manual-invoker/src/main/java/com/example/droolsprototype/NumberAndDelayActionFactory.java
@@ -0,0 +1,17 @@
+package com.example.droolsprototype;
+
+import com.kubiki.controller.commons.actions.dtos.NumberAndDelayActionArgs;
+import com.kubiki.controller.commons.definitons.ActionScheduleRequest;
+import com.kubiki.controller.sample.actions.NumberAndDelayAction;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class NumberAndDelayActionFactory {
+
+    public static Stream<ActionScheduleRequest> getActions(int count, int priority, int delay, long window) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> new NumberAndDelayAction(new NumberAndDelayActionArgs(0), window))
+                .map(a -> new ActionScheduleRequest(a, "key", priority, delay));
+    }
+}
