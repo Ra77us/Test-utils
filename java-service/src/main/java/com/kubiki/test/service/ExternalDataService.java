07e61675e50e88e8a525f21d8d16cd16bd2e2243
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2024-05-06 17:49:20 +0200

    add custom metric

--- java-service/src/main/java/com/kubiki/test/service/ExternalDataService.java
+++ java-service/src/main/java/com/kubiki/test/service/ExternalDataService.java
@@ -1,7 +1,9 @@
 package com.kubiki.test.service;
 
 import com.kubiki.test.dto.SampleDto;
-import lombok.NoArgsConstructor;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,22 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @Service
-@NoArgsConstructor
 @Log4j2
 public class ExternalDataService {
+
+    private final MeterRegistry meterRegistry;
+    private final Counter counter;
 
     public static String DATA_PATH = "/test/get-data";
 
     public static boolean useSlowPath = true;
+
+    public ExternalDataService(MeterRegistry registry) {
+        this.meterRegistry = registry;
+        this.counter = Counter.builder("external-data-incoming-requests-count")
+                .description("a number of requests to /test/get-data-external endpoint")
+                .register(meterRegistry);
+    }
 
     @Value("${test2.slow.path}")
     private String slowPath;
@@ -24,6 +35,7 @@ public class ExternalDataService {
     private String fastPath;
 
     public List<SampleDto> getDataFromExternalSource() {
+        counter.increment();
         String dataSource = useSlowPath ? slowPath : fastPath;
         log.info("providing data from {}", dataSource + DATA_PATH);
         return new RestTemplate().getForObject(dataSource + DATA_PATH, List.class);
